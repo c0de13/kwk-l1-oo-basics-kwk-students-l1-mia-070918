e2959b43e668a469e59ec5dd6a822f5c46d89b2c
c0de13 <meganmpuppy@gmail.com>
2018-07-13 13:07:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -52,7 +52,7 @@ class Shoe
 #///////////////////////////////////////////////////////////////////////////////////////  
 
   def cobble
-    puts "the shoe has been repaired"
+    puts "Your shoe is as good as new!"
   end  
   
   
